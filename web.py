9f16f596105e92131fc77eecd108fb0f3216acfe
g1tpig <138865172+g1tpig@users.noreply.github.com>
2023-09-11 21:50:41 +0800

    Update web.py

--- web.py
+++ web.py
@@ -8,13 +8,7 @@ bg_css = """
 .stApp {
   background-image: url('https://images.unsplash.com/photo-1630438325568-69fc918b27cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3131&q=80');
   background-size: cover;
-  height: 50%;
-  width: 100%;
   background-attachment: fixed;
-  filter: blur(5px) opacity(0.5); 
-  position: absolute;
-  top: 0;
-  left: 0; 
 }
 </style>
 """
