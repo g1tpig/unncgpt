974bcdf82fab396c40ea794841c6c66bfe2602d7
g1tpig <138865172+g1tpig@users.noreply.github.com>
2023-09-11 20:44:51 +0800

    Update web.py

--- web.py
+++ web.py
@@ -14,6 +14,8 @@ if "openai_model" not in st.session_state:
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    with st.chat_message("assistant"):
+        st.markdown("Hi!Welcome to UNNC🥰")
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
