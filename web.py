2eb5e25241edd54c8d65907210a6364b6e26cc6b
g1tpig <138865172+g1tpig@users.noreply.github.com>
2023-09-11 21:08:46 +0800

    Update web.py

--- web.py
+++ web.py
@@ -21,6 +21,8 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+print(st.session_state.messages)
+
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
