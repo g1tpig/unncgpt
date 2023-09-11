00b24730e3d1858003de7bf439f1791fee735dd6
g1tpig <138865172+g1tpig@users.noreply.github.com>
2023-09-11 21:52:58 +0800

    Update web.py

--- web.py
+++ web.py
@@ -6,9 +6,10 @@ from datetime import date
 bg_css = """  
 <style>
 .stApp {
-  background-image: url('https://images.unsplash.com/photo-1630438325568-69fc918b27cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3131&q=80');
+  background-image: url('https://images.unsplash.com/photo-1630438325568-69fc918b27cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3131&q=80?blur=5px&opacity=0.5'');
   background-size: cover;
   background-attachment: fixed;
+  background-position: center;
 }
 </style>
 """
